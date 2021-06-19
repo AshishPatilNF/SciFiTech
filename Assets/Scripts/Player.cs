2ddf506859d996ffe8af9452af53c3093b7e9d93
Ashish Patil <ashishsanjaypatil@gmail.com>
2021-06-19 15:35:37 +0530

    Added primitive Player with CharacterController movement and jump functionality

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 5f;
+
+    float jumpHeight = 8f;
+
+    float customGravity = 1f;
+
+    float yVelocity = 0;
+
+    bool canDoubleJump = false;
+
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 customVelocity = moveDirection * speed;
+
+        if (characterController.isGrounded)
+        {
+            canDoubleJump = true;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                yVelocity = jumpHeight;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && canDoubleJump)
+            {
+                canDoubleJump = false;
+                yVelocity += jumpHeight;
+            }
+
+            yVelocity -= customGravity;
+        }
+
+        customVelocity.y = yVelocity;
+        characterController.Move(customVelocity * Time.deltaTime);
+    }
+}
