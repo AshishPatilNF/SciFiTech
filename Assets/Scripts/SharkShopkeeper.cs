8047ac167214121a96b4dacb33ecb467c5e60498
Ashish Patil <ashishsanjaypatil@gmail.com>
2021-06-23 13:09:56 +0530

    Added SFX for buying weapon

--- Assets/Scripts/SharkShopkeeper.cs
+++ Assets/Scripts/SharkShopkeeper.cs
@@ -7,8 +7,12 @@ public class SharkShopkeeper : MonoBehaviour
     [SerializeField]
     GameObject weaponForSale;
 
+    [SerializeField]
+    AudioClip boughtWeaponSound;
+
     public GameObject PurchasedWeapon()
     {
+        AudioSource.PlayClipAtPoint(boughtWeaponSound, Camera.main.transform.position);
         return weaponForSale;
     }
 }
